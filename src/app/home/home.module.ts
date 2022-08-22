2895e5e324aa0eac820539e6f7b3b29581329a27
gersancabo <gersancabo@gmail.com>
2022-08-22 12:23:38 +0100

    starting 'about me' section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutMeComponent
   ],
   imports: [
     CommonModule
