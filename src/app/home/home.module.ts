991afef8c9692d45dc1e4f60da8eaab1a687e9cf
gersancabo <gersancabo@gmail.com>
2022-09-02 22:03:47 +0100

    added footer and added a style rule into header for landscape screens

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
