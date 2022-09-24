7db71a271d754eea587d64031b8d1f2c38ba719d
gersancabo <gersancabo@gmail.com>
2022-09-24 21:47:26 +0100

    making progress in about me section

--- src/app/home/components/about-me/about-me.component.ts
+++ src/app/home/components/about-me/about-me.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  //Array with
+  /*
+  arrayTechnologies: Array<string> = [
+    "JavaScript",
+    "PHP",
+    "Java",
+    "SQL",
+    "Angular",
+    "React",
+    "Docker",
+    "Git",
+    "CSS",
+    "JSON",
+    "XML",
+    "Linux",
+    "Unit Testing"
+  ]
+  */
+
   constructor() { }
 
   ngOnInit(): void {
+    //this.createDivsTechnologies();
   }
 
+  /*
+  createDivsTechnologies() {
+    this.arrayTechnologies.forEach(technology => {
+      let spanTechnology = document.createElement("div");
+      spanTechnology.textContent = technology;
+      spanTechnology.style.position = "absolute";
+      spanTechnology.style.top = (100 * Math.random()) + "%";
+      spanTechnology.style.left = (100 * Math.random()) + "%";
+      document.getElementById("prov")?.appendChild(spanTechnology);
+    });
+  }
+  */
 }
